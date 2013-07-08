7dd4b81050cdee81f2ac71673851e9c06bb4aa5f
Andreas Schneider <asn@cryptomilk.org>
2013-07-08 17:38:32 +0200

    ctest: Fix CDash project name.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,9 +1,9 @@
 set(UPDATE_TYPE "true")
 
-set(CTEST_PROJECT_NAME "socket_wrapper")
+set(CTEST_PROJECT_NAME "socketwrapper")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 CET")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "mock.cryptomilk.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=socket_wrapper")
+set(CTEST_DROP_LOCATION "/submit.php?project=socketwrapper")
 set(CTEST_DROP_SITE_CDASH TRUE)
